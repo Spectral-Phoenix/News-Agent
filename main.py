8503293480480fdc9ba751033e51cc6cf765f3a5
Praneel <140725262+Spectral-Phoenix@users.noreply.github.com>
2024-02-25 04:12:46 +0000

    new file:   2024-02-25_TechCrunch.json 	modified:   main.py 	new file:   templates/index.html

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 from core.exp_github import update
 from core.generate import summarise
 from core.scrape import scrape_articles
 
-#app = Flask(__name__)
-#
-#@app.route('/process', methods=['GET'])
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/process', methods=['GET'])
 def process_data():
     # Call all the functions here
     scrape_articles()
@@ -15,6 +19,5 @@ def process_data():
     
     return jsonify({"message": "Scraping, summarization, and update completed."})
 
-#if __name__ == '__main__':
-    #app.run(debug=True)
-process_data()
+if __name__ == '__main__':
+    app.run(debug=True)
