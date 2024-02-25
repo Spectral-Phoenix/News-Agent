83e09d8b632490c4a0f9f6207afefc6cc8207699
Praneel <140725262+Spectral-Phoenix@users.noreply.github.com>
2024-02-25 07:55:33 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 
-from core.exp_github import update
-from core.generate import summarise
-from core.scrape import scrape_articles
+#from core.exp_github import update
+#from core.generate import summarise
+#from core.scrape import scrape_articles
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "<h2> News Agent - Flask App</h2>"
 
-@app.route('/process', methods=['GET'])
-def process_data():
-    scrape_articles()
-    summarise()
-    update()
-    
-    return jsonify({"message": "Scraping, summarization, and update completed."})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#@app.route('/process', methods=['GET'])
+#def process_data():
+#    scrape_articles()
+#    summarise()
+#    update()
+#    
+#    return jsonify({"message": "Scraping, summarization, and update completed."})
+#
