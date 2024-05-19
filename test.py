450bd3fadc83a6bc0a981d95f0fa0a1ebd26f198
spectralphoenix.07 <spectralphoenix.07@gmail.com>
2024-05-19 12:08:34 +0000

    modified:   core/hindu.py 	deleted:    core/hindu/2024-05-19_the_hindu.json 	deleted:    core/hindu/github.py 	deleted:    core/hindu/supabase.py 	new file:   data/the_hindu/2024-05-18_the_hindu.json 	new file:   data/the_hindu/2024-05-19_the_hindu.json 	new file:   data/the_hindu/blacklist.json 	modified:   test.py

--- test.py
+++ test.py
@@ -1,84 +1,136 @@
 import json
 import logging
-from datetime import date
-from typing import Dict, List
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from datetime import date, datetime
+from typing import Dict, List, Set
 
 import requests
 from bs4 import BeautifulSoup
-from newspaper  import Article
+from newspaper import Article
+
+from core.supa_base import upload
 
 logging.basicConfig(level=logging.ERROR)
 
-def is_blacklisted(url: str, blacklist: List[str]) -> bool:
+BLACKLIST_FILE_PATH = 'data/the_hindu/blacklist.json'
 
-    domain = url.split('//')[-1].split('/')[0]
+def is_blacklisted(url: str, blacklist: List[str]) -> bool:
+    domain = url.split('//')[1].split('/')[0]
     return domain in blacklist
 
-def scrape_links(url: str, blacklist: List[str] = None) -> set[str]:
-
+def scrape_links(url: str, blacklist: List[str] = None) -> Set[str]:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    links = {link['href'] for link in soup.find_all('a') if link.get('href') and link['href'].endswith('.ece')}
-
+    links = {link['href'] for link in soup.find_all('a', href=True) if link['href'].endswith('.ece')}
     if blacklist:
         links = {link for link in links if not is_blacklisted(link, blacklist)}
-
     return links
 
 def scrape_text(link: str) -> Dict[str, str]:
-
     try:
         article = Article(link, language='en')
         article.download()
         article.parse()
-        article_date = article.publish_date.date()
-        # images = article.top_image
-        
+        article_date = article.publish_date.date() if article.publish_date else None
         if article_date == date.today():
             return {
                 'title': article.title,
-                'publish_date': article.publish_date.strftime('%Y-%m-%d'),
+                'publish_date': article.publish_date.strftime('%Y-%m-%d') if article.publish_date else 'N/A',
                 'text': article.text,
                 'url': link,
-                # 'images': images
             }
     except Exception:
         logging.error(f"Unexpected error occurred while scraping {link}")
     return None
 
-def main():
-
-    urls = ["https://techcrunch.com/category/artificial-intelligence", 
-                    "https://techcrunch.com/category/apps", "https://techcrunch.com/category/biotech-health", "https://techcrunch.com/category/climate", "https://techcrunch.com/category/commerce",
-                  "https://techcrunch.com/category/enterprise", "https://techcrunch.com/category/fintech", "https://techcrunch.com/category/gadgets", "https://techcrunch.com/category/gaming", "https://techcrunch.com/category/government-policy", "https://techcrunch.com/category/hardware",
-                  "https://techcrunch.com/category/media-entertainment", "https://techcrunch.com/category/privacy", "https://techcrunch.com/category/robotics", "https://techcrunch.com/category/security", "https://techcrunch.com/category/social", "https://techcrunch.com/category/space",
-                  "https://techcrunch.com/category/startups", "https://techcrunch.com/category/transportation", "https://techcrunch.com/category/venture"]
-                  
-    url_list = set()
-
+def scrape_links_from_urls(urls: List[str], blacklist: List[str]) -> Set[str]:
+    all_links = set()
     for url in urls:
-        links = scrape_links(url)
-        print(f"No of Links: {len(links)}")
-        url_list.update(links)
-    
-    # with open('links.txt', 'w') as f:
-    #     for link in url_list:
-    #         f.write(f"{link}\n")
-    #     print("Links Saved to links.txt")
+        all_links.update(scrape_links(url, blacklist))
+    return all_links
 
-    scraped_data = []
-    for i, link in enumerate(url_list, start=1):
-        print(f"Scraping {i} out of {len(url_list)}")
-        text_data = scrape_text(link)
-        if text_data:
-            scraped_data.append(text_data)
+def load_previous_urls(file_path: str) -> Set[str]:
+    if not os.path.exists(file_path):
+        return set()
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+        return {article['url'] for article in data.get('articles', [])}
+
+def load_blacklist(file_path: str) -> Set[str]:
+    if not os.path.exists(file_path):
+        return set()
+    with open(file_path, 'r') as f:
+        return set(json.load(f))
+
+def save_blacklist(file_path: str, blacklist: Set[str]):
+    with open(file_path, 'w') as f:
+        json.dump(list(blacklist), f, ensure_ascii=True, indent=4)
+
+def main():
+    start_time = datetime.now()
+
+    urls = [
+        'https://www.thehindu.com/news/national/', 'https://www.thehindu.com/news/international/', 'https://www.thehindu.com/news/national/andhra-pradesh/',
+        'https://www.thehindu.com/news/national/karnataka/', 'https://www.thehindu.com/news/national/kerala/', 'https://www.thehindu.com/news/national/tamil-nadu/',
+        'https://www.thehindu.com/news/national/telangana/', 'https://www.thehindu.com/news/cities/Vijayawada/', 'https://www.thehindu.com/business/',
+        'https://www.thehindu.com/business/agri-business/', 'https://www.thehindu.com/business/Economy/', 'https://www.thehindu.com/business/Industry/',
+        'https://www.thehindu.com/business/markets/', 'https://www.thehindu.com/business/budget/', 'https://www.thehindu.com/sci-tech/',
+        'https://www.thehindu.com/sci-tech/science/', 'https://www.thehindu.com/sci-tech/technology/', 'https://www.thehindu.com/sci-tech/health/',
+        'https://www.thehindu.com/sci-tech/agriculture/', 'https://www.thehindu.com/sci-tech/energy-and-environment/', 'https://www.thehindu.com/sci-tech/technology/gadgets/',
+        'https://www.thehindu.com/sci-tech/technology/internet/'
+    ]
+
+    blacklist_domains = ['sportstar.thehindu.com', 'thehindubusinessline.com']
+
+    # Load URL blacklist
+    url_blacklist = load_blacklist(BLACKLIST_FILE_PATH)
+
+    # Collect all links first
+    url_list = scrape_links_from_urls(urls, blacklist_domains)
+
+    # Load previously scraped URLs
+    previous_file_path = f"data/the_hindu/{str(date.today())}_the_hindu.json"
+    previous_urls = load_previous_urls(previous_file_path)
+
+    # Filter out previously scraped and blacklisted URLs
+    new_urls = url_list - previous_urls - url_blacklist
+
+    data = {"source": "the-hindu", "articles": []}
+
+    # Use ProcessPoolExecutor to scrape articles concurrently
+    with ProcessPoolExecutor(max_workers=10) as executor:
+        future_to_url = {executor.submit(scrape_text, link): link for link in new_urls}
+        for i, future in enumerate(as_completed(future_to_url), start=1):
+            link = future_to_url[future]
+            try:
+                text_data = future.result()
+                if text_data:
+                    data["articles"].append(text_data)
+                else:
+                    url_blacklist.add(link)  # Add to blacklist if not from today's date
+                print(f"Scraped {i} out of {len(new_urls)}")
+            except Exception as exc:
+                logging.error(f"Error scraping {link}: {exc}")
+
+    data["no_of_articles"] = len(data["articles"])
 
     today = str(date.today())
-    file_path =  f"{today}_the_hindu.json"
+    file_path = f"{today}_the_hindu.json"
 
-    with open(f'core/hindu/{file_path}', 'w') as f:
-        f.write(scraped_data)
-        print("Scraping Completed")
+    with open(f'data/the_hindu/{file_path}', 'w') as f:
+        json.dump(data, f, ensure_ascii=True, indent=4)
+
+    print("Scraping Completed")
+    # Upload the JSON File to Supabase
+    upload("tech-crunch", file_path, f"data/the_hindu/{file_path}")
+
+    # Save the updated URL blacklist
+    save_blacklist(BLACKLIST_FILE_PATH, url_blacklist)
+
+    end_time = datetime.now()
+    total_time = end_time - start_time
+    print(f"Total time taken: {total_time}")
 
 if __name__ == "__main__":
     main()
