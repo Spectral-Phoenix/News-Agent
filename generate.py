321256b4eff6520119b678ab63ce6a8f3b2d3447
spectralphoenix.07 <spectralphoenix.07@gmail.com>
2024-06-07 03:53:16 +0000

    Initial Commit

--- generate.py
+++ generate.py
@@ -1,188 +1,114 @@
 import json
-import time
-from datetime import date
-from langchain_google_genai import ChatGoogleGenerativeAI
-from supa_base import upload
-from openai import OpenAI
+import logging
+import os
 
-today = str(date.today())
+import cohere
+import google.generativeai as genai
+from dotenv import load_dotenv
 
-client = OpenAI(
-  base_url = "https://integrate.api.nvidia.com/v1",
-  api_key = "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"
-)
+# Load environment variables
+load_dotenv()
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
-    google_api_key="AIzaSyCVamDnNAeezeywfPTet9t_Fvd_DTTIuu0",
-)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
-with open(f'data/the_hindu/{today}_the_hindu.json', 'r') as f:
-    data = json.load(f)
+co = cohere.Client(os.environ.get('COHERE_API_KEY'))
 
-articles = data['articles']
+def configure_generative_model():
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-def summarize_article(article):
+    generation_config = {
+        "temperature": 0.9,
+        "top_p": 0.1,
+        "top_k": 1,
+        "max_output_tokens": 2048,
+    }
+    safety_settings = [
+        {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+        {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+        {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+        {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
+    ]
+    return genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+    )
 
-    prompt = f"""
-        Your task is to summarize the given article text into 3 concise bullet points. Follow these guidelines:
+def cohere_summarize(content):
 
-        - Read the article carefully and identify the main ideas or key points.
-        - Summarize each main idea or key point in a clear, concise sentence.
-        - Keep each bullet point brief, ideally no more than 20 words.
-        - Do not include unnecessary details or examples from the article.
-        - Write in an objective and impartial tone, avoiding personal opinions or biases.
-        - Use proper grammar, spelling, and punctuation.
-        - Donot include any markdown formatting like ```, #, *,etc.
+    response = co.summarize( 
+      text=content,
+      length='medium',
+      format='bullets',
+      model='command-r-plus',
+      additional_command='',
+      temperature=0.3,
+    ) 
+    return response.summary
 
-        Input:
+def cohere_title(question):
 
-        Article: {article['title']}
+    response = co.generate(
+      model='command-r-plus',
+      prompt= question,
+      max_tokens=300,
+      temperature=0.9,
+      k=0,
+      stop_sequences=[],
+      return_likelihoods='NONE')
 
-        Text: {article['text']}
+    return response.generations[0].text
 
-        Summary:
+def load_json_data(json_data):
+    return json.loads(json_data)
 
-        - [Bullet point 1]
-        - [Bullet point 2]
-        - [Bullet point 3]
-    """
+def save_json_data(data, file_path):
+    with open(file_path, "w", encoding="utf-8") as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=2)
 
-    response = llm.invoke(prompt)
-    return response.content
+def generate_summary(model, article_content):
+    prompt = f"{article_content}\n---\nYour task is to summarize the above article into 3 bullet points. Try to include the most important information which provides an overview of the article.\n---\n"
+    try:
+        answer = model.generate_content(prompt)
+        return answer.text.strip().replace("\n\n", "\n")
+    except Exception:
+        logging.error("Error: Summary generation failed, Switching to Fallback Model")
+        fallback_content = cohere_summarize(prompt)
+        return fallback_content
 
-def categorise(article):
-    prompt = f"""
-        Students preparing for competitive exams like UPSC often rely on newspapers for current affairs, but some articles simply do not contain any useful information.
-        Your task is to categorize articles from 'The Hindu' newspaper into the following categories and return a JSON Output:
+def update_json_with_summaries(json_data, model):
+    for article in json_data["articles"]:
+        article["Summary"] = generate_summary(model, article["content"])
 
-        Categories:
-        - World: Any events that occurred outside India
-        - National: Events that occurred in India
-        - Business: News related to companies and startups
-        - Technology: News related to tech companies and their products
-        - Science: Scientific news and discoveries
-        - Sports
-        - Entertainment
-        - Culture
-        - Lifestyle
-        - Politics: News related to political parties, elections, etc.
-        - Economy: Financial news
-        - Health
-        - Education
-        - Environment
-        - Crime
-        - Weather
-        - Traffic
-        - Local
-
-        Guidelines:
-        1. Categorize the article into one or more relevant categories.
-
-        2. Mark an article as "important" if it meets the following criteria:
-
-            The article covers a current affairs topic that is directly relevant to the UPSC syllabus or exam pattern.
-            The topic is related to India's national interest, international relations, economy, science and technology, environment, education, health, or any other subject area that is a part of the UPSC curriculum.
-            The article provides in-depth analysis, facts, figures, or insights that can enhance the aspirant's understanding of the topic.
-            The article discusses a significant event, policy, or decision that has far-reaching implications for the country or the world.
-
-        3. Mark an article as "not important" if it falls into any of the following categories:
-
-            Politics: Articles focusing solely on political parties, elections, or partisan politics.
-            Crime: Articles covering criminal incidents or law enforcement activities, unless they involve high-profile cases or have broader implications for society or governance.
-            Sports: Articles related to sports events, players, or teams, unless they cover a major international event or a sports policy issue.
-            Entertainment: Articles about movies, music, celebrities, or the entertainment industry.
-            Culture: Articles on cultural events, festivals, or traditions, unless they discuss a significant cultural issue or policy.
-            Lifestyle: Articles covering fashion, food, travel, or personal interests.
-            Weather: Articles reporting routine weather updates or forecasts.
-            Traffic: Articles focusing on local traffic conditions or updates.
-            Local: Articles covering hyperlocal events or issues that have limited broader relevance.
-            World: Articles focusing on world events that are not directly related to India or the UPSC syllabus.
-
-            Additionally, you can mark an article as "not important" if it lacks depth, analysis, or meaningful insights, and merely provides a surface-level or sensational account of an event or issue.
-            Strictly mark articles as not important if they are analysis, editorials, essays or something like that.
-            
-        4. Do not provide any additional information or explanations; only return the JSON content.
-        5. Do not add any markdown formatting for the JSON content like ```json, #, *,etc.; return it in plain text format.
-
-        Input :
-        -\-\-
-        Title: {article['title']}
-        Article: {article['text']}
-
-        -\-\-
-
-        Output Format (JSON):
-        {{
-          "category": ["category1", "category2", ...],
-          "relevance": "important" or "not important"
-        }}
-
-        Output:
-
-    """
-    # Add Prompt Length management
-
-    completion = client.chat.completions.create(
-      model="microsoft/phi-3-small-8k-instruct",
-      messages=[{"role":"user","content":f"{prompt}"}],
-      temperature=0.4,
-      top_p=0.7,
-      max_tokens=1024,
-      stream=False
+def generate_revised_title(model, article_title, article_content):
+    revised_title_prompt = (
+        f"Title:\n{article_title}\nContent:\n{article_content}\n---\nYour task is to give a revised title for the above article, do not include any clickbait words and represent the actual content of the article\n---\n"
     )
     try:
-        response = completion.choices[0].message.content
-        category_data = json.loads(response)
-        
-        if isinstance(category_data, list) and len(category_data) > 0:
-            category_data = category_data[0]
-            
-        return category_data['category'], category_data['relevance']
-    except json.JSONDecodeError:
-        print(f"Error parsing JSON for article: {article['title']}")
-        return None, None
-    except (TypeError, KeyError) as e:
-        print(f"Error accessing response data: {e}")
-        return None, None
+        response = model.generate_content(revised_title_prompt)
+        return response.text.strip()
+    except Exception:
+        logging.error("Error: Title generation failed, Switching to Fallback Model")
+        fallback_title_content = cohere_title(revised_title_prompt)
+        return fallback_title_content
 
-def main():
-    count = 0
-    length = len(articles)
+def update_json_with_titles(json_data, model):
+    for article in json_data["articles"]:
+        article["revised_title"] = generate_revised_title(model, article["title"], article["content"])
 
-    for article in articles:
-        print(f"Categorising article {count+1}/{length}")
-        try:
-          category, relevance = categorise(article)
-        except Exception as e:
-          print(f"Error categorising article: {e}")
-          category = "Unknown"
-          relevance = 0
+def load_json_data_1(file_path):
+    with open(file_path, 'r') as file:
+        json_data = json.load(file)
+    return json_data
 
-        if category and relevance:
-            article["category"] = category
-            article["relevance"] = relevance
-            print(category, relevance)
-        else:
-            print(f"Skipping article: {article['title']}")
-        count += 1
-        time.sleep(1)
-
-    count = 0
-    length = len(articles)
-
-    for article in articles:
-        print(f"Summarising article {count+1}/{length}")
-        summary = summarize_article(article)
-        article["summary"] = summary.split("\n")
-        count += 1
-
-    with open(f'data/the_hindu/{today}_the_hindu.json', 'w') as f:
-        json.dump(data, f, indent=4)
-
-    file_path = f"{today}_the_hindu.json"
-    upload("tech-crunch", file_path, f"data/the_hindu/{file_path}")
-    print("Summaries Updated to Supabase")
+def summarise():
+    json_file_path = "techcrunch_articles_2024-06-05.json" 
+    json_data = load_json_data_1(json_file_path)
+    model = configure_generative_model()
+    update_json_with_summaries(json_data, model)
+    update_json_with_titles(json_data, model)
+    save_json_data(json_data, json_file_path) 
 
 if __name__ == "__main__":
-    main()
+    summarise()
--- scrape.py
+++ scrape.py
@@ -1,107 +1,172 @@
 import json
 import logging
-from datetime import date
-from typing import Dict, List
+import newspaper
+import re
+import time
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
-from newspaper import Article
 
-from supa_base import upload
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(level=logging.ERROR)
+def extract_date(link):
+    """
+    Extract the date from a URL.
 
-def is_blacklisted(url: str, blacklist: List[str]) -> bool:
-    domain = url.split('//')[-1].split('/')[0]
-    return domain in blacklist
+    Parameters:
+        link (str): URL string.
 
-def scrape_links(url: str, blacklist: List[str] = None) -> set[str]:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    links = {link['href'] for link in soup.find_all('a') if link.get('href') and link['href'].endswith('.ece')}
-    if blacklist:
-        links = {link for link in links if not is_blacklisted(link, blacklist)}
-    return links
+    Returns:
+        datetime.date or None: Extracted date or None if not found.
+    """
+    match = re.search(r"/(\d{4}/\d{2}/\d{2})/", link)
+    if match:
+        date_str = match.group(1)
+        return datetime.strptime(date_str, "%Y/%m/%d").date()
+    else:
+        return None
 
-def scrape_text(link: str) -> Dict[str, str]:
+def scrape_links(category_url, category):
+    """
+    Scrape links from a specified category URL.
+
+    Parameters:
+        category_url (str): URL of the category.
+        category (str): Category name.
+
+    Returns:
+        list: List of tuples containing (link, category).
+    """
     try:
-        article = Article(link, language='en')
+        resp = requests.get(category_url)
+        resp.raise_for_status()
+        soup = BeautifulSoup(resp.text, 'lxml')
+        return [(a['href'], category) for a in soup.select('h2 a')]
+    except requests.exceptions.RequestException as req_exc:
+        logging.error(f"Error scraping {category_url}: {req_exc}")
+        return []
+
+
+def parse_article(link):
+    """
+    Parse article content from a specified article URL using newspaper3k.
+
+    Parameters:
+        link (str): URL of the article.
+
+    Returns:
+        dict or None: Parsed article data or None if parsing fails.
+    """
+    try:
+        article = newspaper.Article(link)
         article.download()
         article.parse()
-        article_date = article.publish_date.date()
-        images = article.images
-        try:
-            image_link = [image for image in images if image.startswith('https://th-i.thgim.com/')][0]
-        except Exception:
-            image_link = None
-        if article_date == date.today():
 
-            if len(article.text) >= 130:
-                return {
-                    'title': article.title,
-                    'publish_date': article.publish_date.strftime('%Y-%m-%d'),
-                    'text': article.text,
-                    'url': link,
-                    'image' : image_link,
-                }
+        if article.title and article.text:
+            return {
+                "title": article.title,
+                "content": article.text,
+                "image_links": article.top_image,
+                "link": link,
+            }
+        else:
+            logging.error(f"Error parsing article content for {link}: Title or content not found.")
+            return None
+
+    except newspaper.article.ArticleException as article_exc:
+        logging.error(f"Error fetching or parsing article content for {link}: {article_exc}")
+        return None
+
     except Exception as e:
-        logging.error(f"Unexpected error occurred while scraping {link}")
-        print(e)
-    return None
+        logging.error(f"Error parsing article content for {link}: {e}")
+        return None
 
-def main():
-    urls = ['https://www.thehindu.com/news/national/', 'https://www.thehindu.com/news/international/', 'https://www.thehindu.com/business/',
-            'https://www.thehindu.com/business/agri-business/', 'https://www.thehindu.com/business/Economy/', 'https://www.thehindu.com/business/Industry/',
-            'https://www.thehindu.com/business/budget/', 'https://www.thehindu.com/sci-tech/',
-            'https://www.thehindu.com/sci-tech/science/', 'https://www.thehindu.com/sci-tech/technology/', 'https://www.thehindu.com/sci-tech/health/',
-            'https://www.thehindu.com/sci-tech/agriculture/', 'https://www.thehindu.com/sci-tech/energy-and-environment/', 'https://www.thehindu.com/sci-tech/technology/gadgets/',
-            'https://www.thehindu.com/sci-tech/technology/internet/']
-    blacklist = ['sportstar.thehindu.com', 'thehindubusinessline.com']
-    url_list = set()
-    for url in urls:
-        links = scrape_links(url, blacklist)
-        print(f"No of Links: {len(links)}")
-        url_list.update(links)
+def scrape_articles(date_input):
+    """
+    Scrape articles from TechCrunch based on a specified date.
 
-    today = str(date.today())
-    file_path = f"{today}_the_hindu.json"
-    
-    # Check if the file already exists
+    Parameters:
+        date_input (str): Target date in the format 'YYYY-MM-DD'.
+
+    Returns:
+        bytes or None: JSON data in bytes or None if errors occur during the process.
+    """
+    if not date_input:
+        logging.error("Please provide a valid date.")
+        return
     try:
-        with open(f'data/the_hindu/{file_path}', 'r') as f:
-            existing_data = json.load(f)
-            existing_urls = {article['url'] for article in existing_data['articles']}
-    except FileNotFoundError:
-        existing_data = None
-        existing_urls = set()
-    
+        target_date = datetime.strptime(date_input, "%Y-%m-%d").date()
+    except ValueError:
+        logging.error("Invalid date format. Please use YYYY-MM-DD.")
+        return
+
+    # Config
+    BASE_URL = "https://techcrunch.com/category/"
+    CATEGORIES = ["artificial-intelligence", "apps", "biotech-health", "climate", "commerce",
+                  "enterprise", "fintech", "gadgets", "gaming", "government-policy", "hardware",
+                  "media-entertainment", "privacy", "robotics", "security", "social", "space",
+                  "startups", "transportation", "venture"]
+
+    logging.info("Fetching articles...")
+
+    start_time = time.time()
+    links = set()  # Set to store unique links
+
+    for category in CATEGORIES:
+        category_url = BASE_URL + category + "/"
+        category_links = scrape_links(category_url, category)
+
+        for link, category in category_links:
+            date = extract_date(link)
+
+            if date == target_date:
+                links.add(link)
+
+    articles = []
+
+    for link in links:
+        category = None
+        for _, cat in category_links:
+            if _ == link:
+                category = cat
+                break
+
+        article = parse_article(link)
+        # article["category"] = category
+        articles.append(article)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    elapsed_time_in_seconds = int(elapsed_time)
+
+    # Save the data to a JSON file
     data = {
-        "source": "the-hindu",
-        "articles": []
+        "source": "TechCrunch",
+        "date": str(target_date),
+        "no_of_articles": len(articles),
+        "articles": articles
     }
 
-    for i, link in enumerate(url_list, start=1):
-        print(f"Scraping {i} out of {len(url_list)}")
-        if link not in existing_urls:
-            text_data = scrape_text(link)
-            if text_data:
-                data["articles"].append(text_data)
-        else:
-            print(f"Skipping {link} as it already exists in the JSON file.")
+    for article in data["articles"]:
+        if isinstance(article.get("image_links"), set):
+            article["image_links"] = list(article["image_links"]) 
 
-    data["no_of_articles"] = len(data["articles"])
-    
-    # Merge existing data with new data
-    if existing_data:
-        existing_data["articles"].extend(data["articles"])
-        existing_data["no_of_articles"] = len(existing_data["articles"])
-        data = existing_data
+    filename = f"techcrunch_articles_{target_date}.json"
 
-    with open(f'data/the_hindu/{file_path}', 'w') as f:
-        json.dump(data, f, ensure_ascii=True, indent=4)
+    try:
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
 
-    print("Scraping Completed")
-    upload("tech-crunch", file_path, f"data/the_hindu/{file_path}")
+        logging.info(f"{len(articles)} articles scraped successfully and saved to {filename}!")
+        logging.info(elapsed_time_in_seconds)
+        return filename
+
+    except (IOError, OSError) as file_error:
+        logging.error(f"Error writing data to JSON file: {file_error}")
+        return None
 
 if __name__ == "__main__":
-    main()
+    date_input = input("Enter the date (YYYY-MM-DD): ")
+    scrape_articles(date_input)
